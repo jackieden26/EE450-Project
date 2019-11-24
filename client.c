8cd516dcf2ca601f53f40071853f633275ef57ee
ee450 <jackiedeng26@gmail.com>
2019-11-23 16:35:16 -0800

    Make client.c read from argument

--- client.c
+++ client.c
@@ -12,3 +12,25 @@
 
 #define WTP 24700  // AWS TCP port number.
 #define LIP "127.0.0.1"  // Local host IP address.
+
+int main(int argc, char const *argv[]) {
+  if (argc != 4) {
+    printf("wrong number of arguments are typed\n");
+    exit(1);
+  }
+
+  printf("The client is up and running.\n");
+
+  if (strlen(argv[1]) != 1) {
+    printf("map id is more than one letter.\n");
+  }
+
+  // Extract input arguments.
+  char mapid = argv[1][0];
+  int srcVertex = atoi(argv[2]);
+  long int fileSize = atoi(argv[3]);
+
+
+
+  return 0;
+}
